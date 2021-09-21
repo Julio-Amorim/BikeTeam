d2abef4c98868f6859d40d9ea894b76526c064c8
Julio Cesar <juliocesar81632@gmail.com>
2021-09-21 16:49:03 -0300

    main-carousel-1

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+var instance = M.Carousel.init({
+    fullWidth: false,
+    indicators: true
+  });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: false,
+    indicators: true
+  });
